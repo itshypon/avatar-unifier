24dc6fc1363f99332117b806cd6f0342fff961dd
itshypon <himanshupoonia336@gmail.com>
2024-05-30 11:35:56 +0530

    changes all profile photos on feed now

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,17 @@
 const profilePhoto = "https://fastly.picsum.photos/id/199/200/200.jpg?hmac=99686XFipBqxseUxgj_9rxuAbGbc-q9nlzukFHVQyIU"
+const mainImage = document.querySelector("img.global-nav__me-photo");
+mainImage.src = profilePhoto;
 
 function changeProfilePictures() {
-    const elements = document.querySelectorAll('img.EntityPhoto-circle-3, img.EntityPhoto-square-1');
-    elements.forEach((e) => {
-        e.src = profilePhoto;
-    });
+    const images = document.getElementsByTagName('img');
+    for(let img of images) {
+        if(Array.from(img.classList).some(cls => cls.startsWith('EntityPhoto'))) {
+            img.src = profilePhoto;
+        }
+    }
 }
 
-// Run once on page load
+
 changeProfilePictures();
 
 // Set up a MutationObserver to run again whenever new nodes are added to the body
